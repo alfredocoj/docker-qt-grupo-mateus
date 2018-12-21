8b7db2fcbd7c26504943c9f5014e45002d4eab1d
Alfredo <dpd.12qwas>
2018-12-21 17:19:57 -0200

    start.sh para iniciar a aplicacao

--- start.sh
+++ start.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-script_dir=$(pwd)
-echo $script_dir
-echo ${script_dir}
-echo
+echo 'diretorio list'
+DIRS=`find . -maxdepth 3 -type d`
 
-DIRS=`ls -l $script_dir | egrep '^d' | awk '{print $9}'`
+LIBRARY_PATH=``
 
-LD_LIBRARY_PATH=$script_dir
-
+echo 'inicio do for'
 for DIR in $DIRS
 do
-$LIBRARY_PATH=$LIBRARY_PATH':'$script_dir/${DIR}
+LIBRARY_PATH=`$LIBRARY_PATH:$DIR`
+echo $LIBRARY_PATH
 done
 
+echo $LIBRARY_PATH
+
+echo 'expor LD_LIBRARY_PATH'
 export LD_LIBRARY_PATH=$LIBRARY_PATH
 
-#export LD_LIBRARY_PATH=$script_dir:$script_dir/infraestrutura:$script_dir/wms_cd:$script_dir/infrastructure/interface:$script_dir/infrastructure/log:$script_dir/infrastructure/mvc:$script_dir/infrastructure/network/networkwebservice:$script_dir/infrastructure/services:$script_dir/infrastructure/network/networkwebservice:$script_dir/wms_cd_integracao:$script_dir/seguranca:$script_dir/infrastructure/core:$script_dir/infrastructure/db:$script_dir/infrastructure/3rdparty/qtwebapp:$script_dir/infrastructure/network
+#export LD_LIBRARY_PATH=$script_dir:$script_dir/infraestrutura:$script_dir/wms_cd:$script_dir/infrastructure/interface:$script_dir/infrastructure/log:$script_dir/infrastructure/mvc:$script$
 
+echo 'start'
 ${script_dir}/gmcorewmscd/gmcorewmscd wms_cd_webservice
