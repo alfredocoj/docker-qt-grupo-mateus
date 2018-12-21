b087e6a63a74ec495504e31b17b8358dd4ddc51a
Alfredo <dpd.12qwas>
2018-12-21 16:48:34 -0200

    start app qt

--- start.sh
+++ start.sh
@@ -3,15 +3,18 @@
 script_dir=$(pwd)
 echo $script_dir
 echo ${script_dir}
+echo
 
 DIRS=`ls -l $script_dir | egrep '^d' | awk '{print $9}'`
-export LD_LIBRARY_PATH=$script_dir
 
-echo
+LD_LIBRARY_PATH=$script_dir
+
 for DIR in $DIRS
 do
-export LD_LIBRARY_PATH=LD_LIBRARY_PATH:$script_dir/${DIR}
+$LIBRARY_PATH=$LIBRARY_PATH':'$script_dir/${DIR}
 done
+
+export LD_LIBRARY_PATH=$LIBRARY_PATH
 
 #export LD_LIBRARY_PATH=$script_dir:$script_dir/infraestrutura:$script_dir/wms_cd:$script_dir/infrastructure/interface:$script_dir/infrastructure/log:$script_dir/infrastructure/mvc:$script_dir/infrastructure/network/networkwebservice:$script_dir/infrastructure/services:$script_dir/infrastructure/network/networkwebservice:$script_dir/wms_cd_integracao:$script_dir/seguranca:$script_dir/infrastructure/core:$script_dir/infrastructure/db:$script_dir/infrastructure/3rdparty/qtwebapp:$script_dir/infrastructure/network
 
